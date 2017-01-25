44607b0b194f1037bd01dc0e380f8f43b98e9ed0
Hollie Rosser <Hollie Rosser>
2017-01-24 19:30:32 -0600

    adding blog to git repo

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,24 @@
+@extends('app')
+@section('content')
+    <h1>Create Post</h1>
+    {!! Form::open(['url' => 'posts']) !!}
+    <div class="form-group">
+        {!! Form::label ('Title', 'Title:') !!}
+        {!! Form::text ('title', null,['class'=>'form-control']) !!}
+    </div>
+    <div class="form-group">
+        {!!  Form::label('Description', 'Description:') !!}
+        {!!  Form::text('description',null,['class'=>'form-control']) !!}
+    </div>
+    <div class="form-group">
+        {!!  Form::submit('Save', ['class' => 'btn btn-primary form-control']) !!}
+    </div>
+    {!!  Form::close() !!}
+@stop
+
+/**
+ * Created by PhpStorm.
+ * User: Hollie
+ * Date: 1/20/2017
+ * Time: 2:38 PM
+ */
---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,24 @@
+@extends('app')
+@section('content')
+    <h1>Update Post</h1>
+    {!!  Form::model($post,['method' => 'PATCH','route'=>['posts.update',$post->id]]) !!}
+    <div class="form-group">
+        {!!  Form::label('Title', 'Title:') !!}
+        {!!  Form::text('title',null,['class'=>'form-control']) !!}
+    </div>
+    <div class="form-group">
+        {!!  Form::label('Description', 'Description:') !!}
+        {!!  Form::text('description',null,['class'=>'form-control']) !!}
+    </div>
+    <div class="form-group">
+        {!!  Form::submit('Update', ['class'=> 'btn btn-primary']) !!}
+    </div>
+    {!!  Form::close() !!}
+@stop
+
+/**
+ * Created by PhpStorm.
+ * User: Hollie
+ * Date: 1/20/2017
+ * Time: 2:38 PM
+ */
